9443ba934f4d7320698128e171ce3a9d6eb1fa7c
Daniel Braga <dbragabarbosa@gmail.com>
2024-12-27 14:25:45 -0300

    feat: add new converter content

--- converter.py
+++ converter.py
@@ -5,6 +5,12 @@ def metros_para_quilometros(metros):
 def quilometros_para_milhas(quilometros):
     return quilometros * 0.621371
 
+def jardas_para_metros(jardas):
+    return jardas * 0.9144
+
+def milhas_para_metros(milhas):
+    return milhas * 1609.34
+
 # Funções de conversão para peso
 def quilogramas_para_gramas(quilogramas):
     return quilogramas * 1000
@@ -12,9 +18,18 @@ def quilogramas_para_gramas(quilogramas):
 def gramas_para_libras(gramas):
     return gramas * 0.00220462
 
+def oncas_para_gramas(oncas):
+    return oncas * 28.3495
+
+def libras_para_quilogramas(libras):
+    return libras * 0.453592
+
 # Funções de conversão para temperatura
 def celsius_para_fahrenheit(celsius):
     return (celsius * 9/5) + 32
 
 def fahrenheit_para_kelvin(fahrenheit):
     return (fahrenheit - 32) * 5/9 + 273.15
+
+def kelvin_para_fahrenheit(kelvin):
+    return (kelvin - 273.15) * 9/5 + 32
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -14,6 +14,12 @@ def test_metros_para_quilometros():
 def test_quilometros_para_milhas():
     assert quilometros_para_milhas(1) == 0.621371
 
+def test_jardas_para_metros():
+    assert jardas_para_metros(1) == 0.9144
+
+def test_milhas_para_metros():
+    assert milhas_para_metros(1) == 1609.34
+
 # Testes para peso
 def test_quilogramas_para_gramas():
     assert quilogramas_para_gramas(1) == 1000
@@ -21,9 +27,18 @@ def test_quilogramas_para_gramas():
 def test_gramas_para_libras():
     assert gramas_para_libras(1000) == 2.20462
 
+def test_oncas_para_gramas():
+    assert round(oncas_para_gramas(1), 4) == 28.3495
+
+def test_libras_para_quilogramas():
+    assert round(libras_para_quilogramas(1), 6) == 0.453592
+
 # Testes para temperatura
 def test_celsius_para_fahrenheit():
     assert celsius_para_fahrenheit(0) == 32
 
 def test_fahrenheit_para_kelvin():
     assert round(fahrenheit_para_kelvin(32), 2) == 273.15
+
+def test_kelvin_para_fahrenheit():
+    assert round(kelvin_para_fahrenheit(273.15), 2) == 32
