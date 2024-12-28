c22b56742342b54e0376f535931587bbb84c16ed
Daniel Braga <dbragabarbosa@gmail.com>
2024-12-28 00:27:37 -0300

    feat: add new temperature tests

--- converter_temperature.py
+++ converter_temperature.py
@@ -1,9 +1,32 @@
 # Funções de conversão para temperatura
 def celsius_para_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
+    result = (celsius * 9/5) + 32
+    return result
 
 def fahrenheit_para_kelvin(fahrenheit):
-    return (fahrenheit - 32) * 5/9 + 273.15
+    result = (fahrenheit - 32) * 5/9 + 273.15
+    return result
 
 def kelvin_para_fahrenheit(kelvin):
-    return (kelvin - 273.15) * 9/5 + 32
+    result = (kelvin - 273.15) * 9/5 + 32
+    return result
+
+def celsius_para_kelvin(celsius):
+    result = celsius + 273.15
+    return result
+
+def kelvin_para_celsius(kelvin):
+    result = kelvin - 273.15
+    return result
+
+def fahrenheit_para_celsius(fahrenheit):
+    result = (fahrenheit - 32) * 5/9
+    return result
+
+def celsius_para_rankine(celsius):
+    result = (celsius + 273.15) * 9/5
+    return result
+
+def rankine_para_celsius(rankine):
+    result = (rankine - 491.67) * 5/9
+    return result
--- tests/test_converter_temperature.py
+++ tests/test_converter_temperature.py
@@ -9,3 +9,18 @@ def test_fahrenheit_para_kelvin():
 
 def test_kelvin_para_fahrenheit():
     assert round(converter.kelvin_para_fahrenheit(273.15), 2) == 32
+
+def test_celsius_para_kelvin():
+    assert converter.celsius_para_kelvin(0) == 273.15
+
+def test_kelvin_para_celsius():
+    assert converter.kelvin_para_celsius(273.15) == 0
+
+def test_fahrenheit_para_celsius():
+    assert round(converter.fahrenheit_para_celsius(32), 2) == 0
+
+def test_celsius_para_rankine():
+    assert round(converter.celsius_para_rankine(0), 2) == 491.67
+
+def test_rankine_para_celsius():
+    assert round(converter.rankine_para_celsius(491.67), 2) == 0
