c250b40a2e36f84fa67316ea30e9637890bde81f
Daniel Braga <dbragabarbosa@gmail.com>
2024-12-28 00:48:41 -0300

    feat: add volume content

---
+++ converter_volume.py
@@ -0,0 +1,47 @@
+def litros_para_mililitros(litros):
+    result = litros * 1000
+    return result
+
+def mililitros_para_litros(mililitros):
+    result = mililitros / 1000
+    return result
+
+def litros_para_galoes(litros):
+    result = litros * 0.264172
+    return result
+
+def galoes_para_litros(galoes):
+    result = galoes / 0.264172
+    return result
+
+def metros_cubicos_para_litros(metros_cubicos):
+    result = metros_cubicos * 1000
+    return result
+
+def litros_para_metros_cubicos(litros):
+    result = litros / 1000
+    return result
+
+def metros_cubicos_para_galoes(metros_cubicos):
+    result = metros_cubicos * 264.172
+    return result
+
+def galoes_para_metros_cubicos(galoes):
+    result = galoes / 264.172
+    return result
+
+def mililitros_para_galoes(mililitros):
+    result = mililitros * 0.000264172
+    return result
+
+def galoes_para_mililitros(galoes):
+    result = galoes / 0.000264172
+    return result
+
+def litros_para_pintas(litros):
+    result = litros * 2.11338
+    return result
+
+def pintas_para_litros(pintas):
+    result = pintas / 2.11338
+    return result
---
+++ tests/test_converter_volume.py
@@ -0,0 +1,37 @@
+import converter_volume as converter
+
+def test_litros_para_mililitros():
+    assert converter.litros_para_mililitros(1) == 1000
+
+def test_mililitros_para_litros():
+    assert converter.mililitros_para_litros(1000) == 1
+
+def test_litros_para_galoes():
+    assert round(converter.litros_para_galoes(1), 6) == 0.264172
+
+def test_galoes_para_litros():
+    assert round(converter.galoes_para_litros(1), 6) == 3.785412
+
+def test_metros_cubicos_para_litros():
+    assert converter.metros_cubicos_para_litros(1) == 1000
+
+def test_litros_para_metros_cubicos():
+    assert converter.litros_para_metros_cubicos(1000) == 1
+
+def test_metros_cubicos_para_galoes():
+    assert round(converter.metros_cubicos_para_galoes(1), 3) == 264.172
+
+def test_galoes_para_metros_cubicos():
+    assert round(converter.galoes_para_metros_cubicos(1), 6) == 0.003785
+
+def test_mililitros_para_galoes():
+    assert round(converter.mililitros_para_galoes(1000), 9) == 0.000264172
+
+def test_galoes_para_mililitros():
+    assert round(converter.galoes_para_mililitros(1), 0) == 3785
+
+def test_litros_para_pintas():
+    assert round(converter.litros_para_pintas(1), 5) == 2.11338
+
+def test_pintas_para_litros():
+    assert round(converter.pintas_para_litros(1), 6) == 0.473176
