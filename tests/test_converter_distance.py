43e9f849fb2b1635b1776776940bc2fcab6ec792
Daniel Braga <dbragabarbosa@gmail.com>
2024-12-28 22:37:05 -0300

    feat: add new distance tests

--- tests/test_converter_distance.py
+++ tests/test_converter_distance.py
@@ -3,14 +3,32 @@ import converter_distance as converter
 def test_metros_para_quilometros():
     assert converter.metros_para_quilometros(1000) == 1
 
+def test_quilometros_para_metros():
+    assert converter.quilometros_para_metros(1) == 1000
+
 def test_quilometros_para_milhas():
     assert converter.quilometros_para_milhas(1) == 0.621371
+
+def test_milhas_para_quilometros():
+    assert round(converter.milhas_para_quilometros(1), 6) == 1.609344
 
 def test_jardas_para_metros():
     assert converter.jardas_para_metros(1) == 0.9144
 
+def test_metros_para_jardas():
+    assert round(converter.metros_para_jardas(1), 6) == 1.093613
+
+def test_jardas_para_quilometros():
+    assert round(converter.jardas_para_quilometros(1093.613), 6) == 1
+
+def test_quilometros_para_jardas():
+    assert round(converter.quilometros_para_jardas(1), 3) == 1093.613
+
 def test_milhas_para_metros():
     assert converter.milhas_para_metros(1) == 1609.34
+
+def test_metros_para_milhas():
+    assert round(converter.metros_para_milhas(1609.34), 6) == 1
 
 def test_polegadas_para_centimetros():
     assert converter.polegadas_para_centimetros(1) == 2.54
@@ -19,7 +37,38 @@ def test_centimetros_para_polegadas():
     assert converter.centimetros_para_polegadas(2.54) == 1
 
 def test_metros_para_polegadas():
-    assert converter.metros_para_polegadas(1) == 39.3701
+    assert round(converter.metros_para_polegadas(1), 4) == 39.3701
 
 def test_polegadas_para_metros():
-    assert converter.polegadas_para_metros(39.3701) == 1
+    assert round(converter.polegadas_para_metros(39.3701), 4) == 1
+
+def test_polegadas_para_jardas():
+    assert round(converter.polegadas_para_jardas(36), 6) == 1
+
+def test_jardas_para_polegadas():
+    assert converter.jardas_para_polegadas(1) == 36
+
+def test_centimetros_para_metros():
+    assert converter.centimetros_para_metros(100) == 1
+
+def test_metros_para_centimetros():
+    assert converter.metros_para_centimetros(1) == 100
+
+def test_milhas_para_polegadas():
+    assert round(converter.milhas_para_polegadas(1), 2) == 63360
+
+def test_polegadas_para_milhas():
+    assert round(converter.polegadas_para_milhas(63360), 6) == 1
+
+def test_quilometros_para_centimetros():
+    assert converter.quilometros_para_centimetros(1) == 100000
+
+def test_centimetros_para_quilometros():
+    assert converter.centimetros_para_quilometros(100000) == 1
+
+def test_milhas_para_jardas():
+    assert converter.milhas_para_jardas(1) == 1760
+
+def test_jardas_para_milhas():
+    assert converter.jardas_para_milhas(1760) == 1
+
