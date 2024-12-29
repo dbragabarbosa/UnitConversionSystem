950aa5982e87f7ecaa945f01a9707a6a7cd17ff5
Daniel Braga <dbragabarbosa@gmail.com>
2024-12-29 10:23:32 -0300

    fix: fix tests

--- tests/test_converter_velocity.py
+++ tests/test_converter_velocity.py
@@ -1,55 +1,61 @@
 import converter_velocity as converter
 
-def test_metros_por_segundo_para_quilometros_por_hora():
-    assert converter.metros_por_segundo_para_quilometros_por_hora(1) == 3.6
+def test_mps_para_kmh():
+    assert converter.mps_para_kmh(1) == 3.6
 
-def test_quilometros_por_hora_para_metros_por_segundo():
-    assert converter.quilometros_por_hora_para_metros_por_segundo(3.6) == 1.0
+def test_kmh_para_mps():
+    assert converter.kmh_para_mps(3.6) == 1.0
 
-def test_milhas_por_hora_para_quilometros_por_hora():
-    assert converter.milhas_por_hora_para_quilometros_por_hora(1) == 1.609344
+def test_mps_para_mph():
+    assert converter.mps_para_mph(1) == 2.23694
 
-def test_quilometros_por_hora_para_milhas_por_hora():
-    assert converter.quilometros_por_hora_para_milhas_por_hora(1.609344) == 1.0
+def test_mph_para_mps():
+    assert converter.mph_para_mps(2.23694) == 1.0
 
-def test_milhas_por_hora_para_pes_por_segundo():
-    assert round(converter.milhas_por_hora_para_pes_por_segundo(1), 6) == 1.466667
+def test_kmh_para_mph():
+    assert converter.kmh_para_mph(1) == 0.621371
 
-def test_pes_por_segundo_para_milhas_por_hora():
-    assert round(converter.pes_por_segundo_para_milhas_por_hora(1.466667), 6) == 1.0
+def test_mph_para_kmh():
+    assert converter.mph_para_kmh(0.621371) == 1.0
 
-def test_metros_por_segundo_para_pes_por_segundo():
-    assert round(converter.metros_por_segundo_para_pes_por_segundo(1), 6) == 3.28084
+def test_kmh_para_knots():
+    assert converter.kmh_para_knots(1) == 0.539957
 
-def test_pes_por_segundo_para_metros_por_segundo():
-    assert round(converter.pes_por_segundo_para_metros_por_segundo(3.28084), 6) == 1.0
+def test_knots_para_kmh():
+    assert converter.knots_para_kmh(0.539957) == 1.0
 
-def test_milhas_por_hora_para_knot():
-    assert round(converter.milhas_por_hora_para_knot(1), 6) == 0.868976
+def test_mph_para_fts():
+    assert converter.mph_para_fts(1) == 1.46667
 
-def test_knot_para_milhas_por_hora():
-    assert round(converter.knot_para_milhas_por_hora(0.868976), 6) == 1.0
+def test_fts_para_mph():
+    assert converter.fts_para_mph(1.46667) == 1.0
 
-def test_quilometros_por_hora_para_knot():
-    assert round(converter.quilometros_por_hora_para_knot(1), 6) == 0.539957
+def test_mps_para_fts():
+    assert converter.mps_para_fts(1) == 3.28084
 
-def test_knot_para_quilometros_por_hora():
-    assert round(converter.knot_para_quilometros_por_hora(0.539957), 6) == 1.0
+def test_fts_para_mps():
+    assert converter.fts_para_mps(3.28084) == 1.0
 
-def test_metros_por_segundo_para_knot():
-    assert round(converter.metros_por_segundo_para_knot(1), 6) == 1.943844
+def test_knots_para_mps():
+    assert converter.knots_para_mps(1) == 0.514444
 
-def test_knot_para_metros_por_segundo():
-    assert round(converter.knot_para_metros_por_segundo(1.943844), 6) == 1.0
+def test_mps_para_knots():
+    assert converter.mps_para_knots(0.514444) == 1.0
 
-def test_quilometros_por_hora_para_milhas_por_segundo():
-    assert round(converter.quilometros_por_hora_para_milhas_por_segundo(3600), 6) == 0.621371
+def test_knots_para_mph():
+    assert converter.knots_para_mph(1) == 1.15078
 
-def test_milhas_por_segundo_para_quilometros_por_hora():
-    assert round(converter.milhas_por_segundo_para_quilometros_por_hora(0.621371), 6) == 3600
+def test_mph_para_knots():
+    assert converter.mph_para_knots(1.15078) == 1.0
 
-def test_metros_por_segundo_para_milhas_por_hora():
-    assert round(converter.metros_por_segundo_para_milhas_por_hora(1), 6) == 2.236936
+def test_kmh_para_cmps():
+    assert converter.kmh_para_cmps(1) == 27.7778
 
-def test_milhas_por_hora_para_metros_por_segundo():
-    assert round(converter.milhas_por_hora_para_metros_por_segundo(2.236936), 6) == 1.0
+def test_cmps_para_kmh():
+    assert converter.cmps_para_kmh(27.7778) == 1.0
+
+def test_kmh_para_mmin():
+    assert converter.kmh_para_mmin(1) == 16.6667
+
+def test_mmin_para_kmh():
+    assert converter.mmin_para_kmh(16.6667) == 1.0
