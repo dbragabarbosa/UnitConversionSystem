5267f0852a95cc39fccff8fb5580759ac5287ea3
Daniel Braga <dbragabarbosa@gmail.com>
2024-12-28 22:50:09 -0300

    feat: add new space content

--- converter_space.py
+++ converter_space.py
@@ -45,3 +45,75 @@ def acres_para_quilometros_quadrados(acres):
 def quilometros_quadrados_para_acres(quilometros_quadrados):
     result = quilometros_quadrados / 0.0040468564224
     return result
+
+def pes_quadrados_para_metros_quadrados(pes_quadrados):
+    result = pes_quadrados * 0.092903
+    return result
+
+def metros_quadrados_para_pes_quadrados(metros_quadrados):
+    result = metros_quadrados / 0.092903
+    return result
+
+def jardas_quadradas_para_metros_quadrados(jardas_quadradas):
+    result = jardas_quadradas * 0.836127
+    return result
+
+def metros_quadrados_para_jardas_quadradas(metros_quadrados):
+    result = metros_quadrados / 0.836127
+    return result
+
+def milhas_quadradas_para_quilometros_quadrados(milhas_quadradas):
+    result = milhas_quadradas * 2.58999
+    return result
+
+def quilometros_quadrados_para_milhas_quadradas(quilometros_quadrados):
+    result = quilometros_quadrados / 2.58999
+    return result
+
+def polegadas_quadradas_para_centimetros_quadrados(polegadas_quadradas):
+    result = polegadas_quadradas * 6.4516
+    return result
+
+def centimetros_quadrados_para_polegadas_quadradas(centimetros_quadrados):
+    result = centimetros_quadrados / 6.4516
+    return result
+
+def acres_para_jardas_quadradas(acres):
+    result = acres * 4840
+    return result
+
+def jardas_quadradas_para_acres(jardas_quadradas):
+    result = jardas_quadradas / 4840
+    return result
+
+def metros_quadrados_para_milimetros_quadrados(metros_quadrados):
+    result = metros_quadrados * 1_000_000
+    return result
+
+def milimetros_quadrados_para_metros_quadrados(milimetros_quadrados):
+    result = milimetros_quadrados / 1_000_000
+    return result
+
+def metros_quadrados_para_centimetros_quadrados(metros_quadrados):
+    result = metros_quadrados * 10_000
+    return result
+
+def centimetros_quadrados_para_metros_quadrados(centimetros_quadrados):
+    result = centimetros_quadrados / 10_000
+    return result
+
+def hectares_para_jardas_quadradas(hectares):
+    result = hectares * 11_959.900463
+    return result
+
+def jardas_quadradas_para_hectares(jardas_quadradas):
+    result = jardas_quadradas / 11_959.900463
+    return result
+
+def milhas_quadradas_para_acres(milhas_quadradas):
+    result = milhas_quadradas * 640
+    return result
+
+def acres_para_milhas_quadradas(acres):
+    result = acres / 640
+    return result
--- tests/test_converter_space.py
+++ tests/test_converter_space.py
@@ -35,3 +35,39 @@ def test_acres_para_quilometros_quadrados():
 
 def test_quilometros_quadrados_para_acres():
     assert round(converter.quilometros_quadrados_para_acres(1), 6) == 247.105381
+
+def test_pes_quadrados_para_jardas_quadradas():
+    assert converter.pes_quadrados_para_jardas_quadradas(9) == 1
+
+def test_jardas_quadradas_para_pes_quadrados():
+    assert converter.jardas_quadradas_para_pes_quadrados(1) == 9
+
+def test_acres_para_jardas_quadradas():
+    assert round(converter.acres_para_jardas_quadradas(1), 2) == 4840
+
+def test_jardas_quadradas_para_acres():
+    assert round(converter.jardas_quadradas_para_acres(4840), 6) == 1
+
+def test_hectares_para_jardas_quadradas():
+    assert round(converter.hectares_para_jardas_quadradas(1), 6) == 11959.900463
+
+def test_jardas_quadradas_para_hectares():
+    assert round(converter.jardas_quadradas_para_hectares(11959.900463), 6) == 1
+
+def test_milhas_quadradas_para_hectares():
+    assert converter.milhas_quadradas_para_hectares(1) == 258.998811
+
+def test_hectares_para_milhas_quadradas():
+    assert round(converter.hectares_para_milhas_quadradas(258.998811), 6) == 1
+
+def test_milhas_quadradas_para_acres():
+    assert converter.milhas_quadradas_para_acres(1) == 640
+
+def test_acres_para_milhas_quadradas():
+    assert converter.acres_para_milhas_quadradas(640) == 1
+
+def test_metros_quadrados_para_jardas_quadradas():
+    assert round(converter.metros_quadrados_para_jardas_quadradas(1), 6) == 1.195990
+
+def test_jardas_quadradas_para_metros_quadrados():
+    assert round(converter.jardas_quadradas_para_metros_quadrados(1.195990), 6) == 1
